966855cc7614f6e449086b37ed58f17847661459
Leonardo <contatoleonardobaptista@gmail.com>
2024-12-04 20:50:21 -0300

    adding home component

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import { UserSummary } from "../../components/UserSummary";
+import { HomeContainer } from "./styles";
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <UserSummary />
+    </HomeContainer>
+  );
+}

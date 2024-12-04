9ec7c1dc2ce2b69235d7346ae84996969b256238
Leonardo <contatoleonardobaptista@gmail.com>
2024-12-04 20:49:21 -0300

    adding header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+import background from "../../assets/background_header.svg";
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  height: 18.5rem;
+
+  background-image: url(${background});
+  background-size: cover;
+  background-position: center;
+`;

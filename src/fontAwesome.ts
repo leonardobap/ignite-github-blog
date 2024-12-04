4058834762af4723a2309018a936a918e7bf5421
Leonardo <contatoleonardobaptista@gmail.com>
2024-12-04 20:48:55 -0300

    adding icons to use in user summary

---
+++ src/fontAwesome.ts
@@ -0,0 +1,4 @@
+import { faBuilding, faUserGroup, faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+export { faGithub, faBuilding, faUserGroup, faArrowUpRightFromSquare };

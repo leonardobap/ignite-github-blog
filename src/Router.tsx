595aaab12d9902bcbf3c7c6614ca3209dea8d193
Leonardo <contatoleonardobaptista@gmail.com>
2024-12-07 16:48:13 -0300

    adding route about issue

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Default } from "./layouts/Default";
+import { AboutIssue } from "./pages/AboutIssue";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Default />}>
         <Route path="/" element={<Home />} />
+        <Route path="/about-issue" element={<AboutIssue />} />
       </Route>
     </Routes>
   );

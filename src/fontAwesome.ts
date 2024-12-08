ec51b22c01599b3c67551b0ace32543f24c125e3
Leonardo <contatoleonardobaptista@gmail.com>
2024-12-08 14:24:24 -0300

    adding new icons

--- src/fontAwesome.ts
+++ src/fontAwesome.ts
@@ -1,4 +1,20 @@
-import { faBuilding, faUserGroup, faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
-export { faGithub, faBuilding, faUserGroup, faArrowUpRightFromSquare };
+import {
+  faBuilding,
+  faUserGroup,
+  faArrowUpRightFromSquare,
+  faChevronLeft,
+  faCalendarDay,
+  faComment,
+} from "@fortawesome/free-solid-svg-icons";
+
+export {
+  faGithub,
+  faBuilding,
+  faUserGroup,
+  faArrowUpRightFromSquare,
+  faChevronLeft,
+  faCalendarDay,
+  faComment,
+};
